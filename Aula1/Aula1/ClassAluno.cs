75fdd9c29e0774483d0bdbfe38dd79d1442c3f14
Sarah Duarte <sarahf.fduarte@gmail.com>
2020-03-08 22:20:58 -0400

    Reorganizar classe

--- Aula1/Aula1/ClassAluno.cs
+++ Aula1/Aula1/ClassAluno.cs
@@ -15,55 +15,17 @@ namespace Aula1
         private String curso;
         private String endereco;
 
-        public void setNome(String nome)
-        {
-            this.nome = nome;
-        }
-        public String getNome()
-        {
-            return this.nome;
-        }
-        public void setEmail(String email)
-        {
-            this.email = email;
-        }
-        public String getEmail()
-        {
-            return this.email;
-        }
-        public void setMatricula(String matricula)
-        {
-            this.matricula = matricula;
-        }
-        public String getMatricula()
-        {
-            return this.matricula;
-        }
-        public void setTelefone(int telefone)
-        {
-            this.telefone = telefone;
-        }
-        public int getTelefone()
-        {
-            return this.telefone;
-        }
-        public void setCurso(String curso)
-        {
-            this.curso = curso;
-        }
-        public String getCurso()
-        {
-            return this.curso;
-        }
-        public void setEndereco(String endereco)
-        {
-            this.endereco = endereco;
-        }
-        public String getEndereco()
-        {
-            return this.endereco;
-        }
-
-
+        public void setNome(String nome) { this.nome = nome; }
+        public String getNome() { return this.nome; }
+        public void setEmail(String email) { this.email = email; }
+        public String getEmail() { return this.email; }
+        public void setMatricula(String matricula) { this.matricula = matricula; }
+        public String getMatricula() { return this.matricula; }
+        public void setTelefone(int telefone) { this.telefone = telefone; }
+        public int getTelefone() { return this.telefone; }
+        public void setCurso(String curso) { this.curso = curso; }
+        public String getCurso() { return this.curso; }
+        public void setEndereco(String endereco) { this.endereco = endereco; }
+        public String getEndereco() { return this.endereco; }
     }
 }
